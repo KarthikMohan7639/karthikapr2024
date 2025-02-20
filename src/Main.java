fab80ade2dfa1513aa9723d2ff179d13d18dc147
KarthikMohan7639 <karthikmohan7639@gmail.com>
2025-02-20 11:56:28 +0530

    nw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Scanner sc=new Scanner(System.in);
+//        int Marks=sc.nextInt();
+//        System.out.println("Enter your Marks");
+//        if(Marks>=90){
+//            System.out.println("O Grade");
+//        }
+//        else if(Marks>=80){
+//            System.out.println("A+ Grade");
+//        }
+//        else if (Marks>=70){
+//            System.out.println("A Grade");
+//        }
+//        else if(Marks>=60){
+//            System.out.println("B+ Grade");
+//        }
+        switch (1){
+            case 1:{
+                System.out.println("1st element");
+            }
+
+            case 2:{
+                System.out.println("2nd element");
+                //break;
+            }
+
+            case 3:{
+                System.out.println("3rd`element");
+            }
+
+            default:{
+                System.out.println("Invalid");
+            }
+
+
+        }
+        //sc.close();
+        }
     }
-}
---
+++ src/Solution.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter  choice");
+        System.out.println("1.ADD/n2.SUB/n3.MULTIPLY/n4.DIV/n5.MODULO");
+        switch (choice):
+    }
+}
