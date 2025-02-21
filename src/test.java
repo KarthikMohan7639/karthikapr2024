17e685e53ad9368db2716f510d6efeabee19897f
KarthikMohan7639 <karthikmohan7639@gmail.com>
2025-02-21 10:29:13 +0530

    commit

---
+++ src/test.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class test {
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        String name=sc.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
